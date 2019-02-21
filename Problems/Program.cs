3e4d93f96323dd279b7db2d18371609ee6611ab2
William Carpenter-Frank <G17C0339@ict.ru.ac.za>
2019-02-21 15:31:18 +0200

    Added other files

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Problems
 {
-    class Program
+    public class  Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test="Hello, there are five words!";
+
+            string result=Prefix(test);
+            Console.WriteLine(result);
+        }
+
+        public static string Prefix(string input)
+        {            
+            int length =input.Length;
+            int wCount=input.Split(' ').Length;
+            return $"{length},{wCount}:{input}";
         }
     }
 }
