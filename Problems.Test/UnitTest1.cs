626d18781226ff22170ed40a918a6dabdead1fe4
William Carpenter-Frank <G17C0339@ict.ru.ac.za>
2019-02-22 15:58:35 +0200

    Single method Test

--- Problems.Test/UnitTest1.cs
+++ Problems.Test/UnitTest1.cs
@@ -15,18 +15,9 @@ namespace Tests
         {
             Program s= new Program();
             Assert.AreEqual("5,1:Hello", s.Prefix("Hello"));
-        }
-        [Test] 
-        public void Test2Prefix()
-        {
-            Program s= new Program();
             Assert.AreEqual("0,0:", s.Prefix(""));
-        }
-       [Test] 
-        public void Test3Prefix()
-        {
-            Program s= new Program();
             Assert.AreEqual("27,5:what    ...  did you say?? ", s.Prefix("what    ...  did you say?? "));
         }
+
     }
 }
